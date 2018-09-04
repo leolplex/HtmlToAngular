103bb847c0f94b822fef6cbb258e277cb98c252a
Daniel Leonardo Romero Albarracin <danielleonardo.romeroalbarracin@scotiabank.com>
2018-09-04 17:30:39 -0500

    finished section 5

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -9,15 +9,28 @@ export class InfoPageService {
   info: InfoPage = {};
   cargada = false;
 
+  equipo: any[] = [];
+
+
   constructor(private http: HttpClient) {
     // console.log('Servicio de InfoPagina Listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     // leer archivo json
-    this.http.get('assets/data/data-page.json')
-        .subscribe( (resp: InfoPage) => {
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-        });
+    this.http.get('assets/data/data-page.json').subscribe((resp: InfoPage) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo() {
+      // leer archivo json
+    this.http.get('https://angular-html-a6da9.firebaseio.com/equipo.json').subscribe((resp: any) => {
+      this.equipo = resp;
+      console.log(this.equipo);
+    });
   }
 }
