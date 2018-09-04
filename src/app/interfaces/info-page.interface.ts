b8ea264953b318fa882a2c2d94e9c4e0e1b47e72
Daniel Leonardo Romero Albarracin <danielleonardo.romeroalbarracin@scotiabank.com>
2018-09-04 16:53:04 -0500

    finished section 4

---
+++ src/app/interfaces/info-page.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPage {
+  tittle?: string;
+  email?: string;
+  nickname?: string;
+  author_page?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  team_work?: any[];
+}
